70ad947f57820d42d70be951e3a0f8cbf785c801
hasibulislam999 <hasib143sl@gmail.com>
2022-05-01 12:50:14 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 var bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({
+            message: 'unauthorized access'
+        });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({
+                message: 'forbidden access'
+            });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+    // console.log(authHeader);
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.e04hb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -18,7 +40,19 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("foodExpress").collection("service");
+        const orderCollection = client.db("foodExpress").collection("order");
         console.log('Genius Car DB Connected!');
+
+        // AUTH
+        // node
+        // require('crypto').randomBytes(64).toString('hex')
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
+                expiresIn: '1d'
+            });
+            res.send({token});
+        })
 
         // get all services
         app.get('/service', async (req, res) => {
@@ -50,6 +84,31 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
+        })
+
+        // post order api
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            // console.log(order);
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        // order collection api
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            // console.log(email);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            } else {
+                return res.status(403).send({
+                    message: 'forbidden access'
+                });
+            }
         })
     } finally {
         // await client.close();
